2d37c4458ec4c869eb46edc576763ebf66658fef
242-napo <napoleoneshun61@gmail.com>
2026-05-30 17:53:40 -0200

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+#File:initials.py
+# Description:Print my initials in large stylized letters
+# Assignment number: 2
+#
+# Name: Napoleon Eshun Tandoh
+# Student ID: 2425402179
+# Email: napoleoneshun61@gmail.com
+# Grader: Augustus
+
+# On my honour, Napoleon Eshun Tandoh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    #This function Prints my initials in large letters.
+    print()
 
+print("...NET")
 
+print()
+
+print("...NNNN.......NN..........EEEEEEEEEE............TTTTTTTTTTTT")
+print("...NN..NN.....NN..........EE.........................TT....")
+print("...NN...NN....NN..........EE.........................TT....")
+print("...NN....NN...NN..........EE.........................TT....")
+print("...NN.....NN..NN..........EE.........................TT....")
+print("...NN......NN.NN..........EEEEEEEE...................TT....")
+print("...NN........NNN..........EE.........................TT....")
+print("...NN........NNN..........EE.........................TT....")
+print("...NN........NNN..........EE.........................TT....")
+print("...NN........NNN..........EE.........................TT....")
+print("...NN.........NN..........EEEEEEEEEE.................TT....")
+
+print()
 
 main()
